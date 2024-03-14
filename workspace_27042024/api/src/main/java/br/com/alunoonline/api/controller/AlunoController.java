42a8175469a3fd3a0857222ed2c95fb8123f5ce1
Yann Carvalho <yanncarvalho95@gmail.com>
2024-03-14 20:36:48 -0300

    java spring

---
+++ workspace_27042024/api/src/main/java/br/com/alunoonline/api/controller/AlunoController.java
@@ -0,0 +1,21 @@
+package br.com.alunoonline.api.controller;
+
+import br.com.alunoonline.api.model.Aluno;
+import br.com.alunoonline.api.service.AlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/aluno")
+public class AlunoController {
+
+    @Autowired
+    AlunoService alunoService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@RequestBody Aluno aluno) {
+        alunoService.create(aluno);
+    }
+}
---
+++ workspace_27042024/api/src/main/java/br/com/alunoonline/api/service/AlunoService.java
@@ -0,0 +1,17 @@
+package br.com.alunoonline.api.service;
+
+import br.com.alunoonline.api.model.Aluno;
+import br.com.alunoonline.api.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AlunoService {
+
+    @Autowired
+    AlunoRepository alunoRepository;
+
+    public void create(Aluno aluno) {
+        alunoRepository.save(aluno);
+    }
+}
